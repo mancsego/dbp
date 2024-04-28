548bf7fa2fb4b873629642e5f30d63ea59b3a3cc
István Ábrahám <istvan.abraham@vicampo.de>
2024-04-28 13:18:55 +0200

    Refine auditing logic

--- src/main/java/com/codingchallange/dgp/entities/Group.java
+++ src/main/java/com/codingchallange/dgp/entities/Group.java
@@ -4,27 +4,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.Instant;
+import lombok.experimental.Accessors;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Accessors(chain = true)
 @Table(name = "groups")
-@EntityListeners(AuditingEntityListener.class)
-public class Group {
+public class Group extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Boolean isBudget;
-    @CreatedDate
-    private Instant createdAt;
-    @LastModifiedDate
-    private Instant updatedAt;
 }
